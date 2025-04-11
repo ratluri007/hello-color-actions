13c8ff1d55baa54a797e6229d6f4ac8b8a900e99
Ram Atluri <r_atluri@yahoo.com>
2025-04-11 08:34:16 -0400

    commit

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from my home in Charlotte!";
 console.log(hello());
 exports.hello = hello;
